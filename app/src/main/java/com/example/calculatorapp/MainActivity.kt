f2a23db63856f71c99dac55744c2681250e89980
meghnaaravind <A00256942@mycambrian.ca>
2023-07-22 12:13:42 -0400

    implemented code for all the operators and build successful.

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BtnAdd.setOnClickListener {
+            val input1 = EdtInp1.text.toString().toInt()
+            val input2 = EdtInp2.text.toString().toInt()
+            ResultTv1.text = (input1+input2).toString()
+        }
+
+        BtnSub.setOnClickListener {
+            val input1 = EdtInp1.text.toString().toInt()
+            val input2 = EdtInp2.text.toString().toInt()
+            ResultTv1.text = (input1-input2).toString()
+        }
+
+        BtnDiv.setOnClickListener {
+            val input1 = EdtInp1.text.toString().toInt()
+            val input2 = EdtInp2.text.toString().toInt()
+            ResultTv1.text = (input1/input2).toString()
+        }
+
+        BtnMul.setOnClickListener {
+            val input1 = EdtInp1.text.toString().toInt()
+            val input2 = EdtInp2.text.toString().toInt()
+            ResultTv1.text = (input1*input2).toString()
+        }
     }
 }
